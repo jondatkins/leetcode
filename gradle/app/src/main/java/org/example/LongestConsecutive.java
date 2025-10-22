0d7afb1f859e2a91a4dd947af33f66b7f054f352
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-10-22 13:40:28 +0100

    Moving gradle project into 'gradle' dir.

--- gradle/app/src/main/java/org/example/LongestConsecutive.java
+++ gradle/app/src/main/java/org/example/LongestConsecutive.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class LongestConsecutive {
 
 	public static void main(String[] args) {
-		int[] nums = { 100, 4, 200, 1, 3, 2 }; // Should be 4
+		  int[] nums = { 100, 4, 200, 1, 3, 2 }; // Should be 4
 		System.out.println("4");
 		System.out.println(new LongestConsecutive().longestConsecutive(nums));
 		int[] nums2 = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }; // should be 9
